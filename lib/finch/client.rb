825e38d470c7eaed30a2b6678313fb24cddb8fab
Kieran Eglin <kieran.eglin@gmail.com>
2022-01-25 15:15:59 -0800

    Exposed configuration class on main module

--- lib/finch/client.rb
+++ lib/finch/client.rb
@@ -5,6 +5,22 @@ require 'finch/client/configuration'
 
 module Finch
   module Client
+    extend self
+
     class Error < StandardError; end
+
+    def configure
+      yield configuration if block_given?
+    end
+
+    def configuration
+      @configuration ||= Finch::Client::Configuration.new
+    end
+
+    private
+
+    def configuration=(configuration)
+      @configuration = configuration
+    end
   end
 end
--- spec/finch/client_spec.rb
+++ spec/finch/client_spec.rb
@@ -4,4 +4,28 @@ RSpec.describe Finch::Client do
   it 'has a version number' do
     expect(Finch::Client::VERSION).not_to be nil
   end
+
+  describe '#configure' do
+    it 'yields the configuration' do
+      expect { |b| described_class.configure(&b) }.to yield_with_args(Finch::Client::Configuration)
+    end
+  end
+
+  describe '#configuration' do
+    it 'returns the configuration' do
+      expect(described_class.configuration).to be_a(Finch::Client::Configuration)
+    end
+
+    it 'returns custom property values' do
+      described_class.configure do |config|
+        config.client_id = '12345'
+        config.client_secret = 'abcdef'
+        config.sandbox = true
+      end
+
+      expect(described_class.configuration.client_id).to eq('12345')
+      expect(described_class.configuration.client_secret).to eq('abcdef')
+      expect(described_class.configuration.sandbox).to be(true)
+    end
+  end
 end
