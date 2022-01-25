48efe7e12ac8f493aa3edd0e167468f4b701fd5c
Kieran <kieran.eglin@gmail.com>
2022-01-25 15:17:56 -0800

    Gem configuration (#3)
    * Added configuration class with defaults
    
    * Exposed configuration class on main module

--- lib/finch/client.rb
+++ lib/finch/client.rb
@@ -1,10 +1,26 @@
 # frozen_string_literal: true
 
 require 'finch/client/version'
+require 'finch/client/configuration'
 
 module Finch
   module Client
+    extend self
+
     class Error < StandardError; end
-    # Your code goes here...
+
+    def configure
+      yield configuration if block_given?
+    end
+
+    def configuration
+      @configuration ||= Finch::Client::Configuration.new
+    end
+
+    private
+
+    def configuration=(configuration)
+      @configuration = configuration
+    end
   end
 end
